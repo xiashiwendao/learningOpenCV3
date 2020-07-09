22f4cb701c44550d7bb33a27a9e14fab8b8c04f7
Lorry <xiashiwendao@163.com>
2020-07-10 06:37:00 +0800

    add mouse draw rectangle

--- Chapter_8.cpp
+++ Chapter_8.cpp
@@ -1,21 +1,14 @@
-#include "Chapter_8.h"
+﻿#include "Chapter_8.h"
 #include <opencv.hpp>
 #include <iostream>
+#include<stdio.h>
 
 using namespace std;
 using namespace cv;
 
 std::vector<Point> points;
 
-//void on_mouce(int event, int x, int y, int flag, void *ustc) {
-//	static Point pre_pt = Point(-1, -1);
-//	static Point cur_pt = Point(-1, -1);
-//	char temp[16];
-//	if (event == cv::EVENT_LBUTTONDOWN) {
-//		pre_pt = Point(x, y);
-//		putText(img, temp, pre_pt, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1, 8)
-//	}
-//}
+
 
 void exe_1() {
 	VideoCapture cap;
@@ -58,6 +51,79 @@ void exe_1() {
 	cap.release();
 
 }
+Mat img = imread("C:\\Users\\python_gay\\Pictures\\download.jpg");
+void on_mouce(int event, int x, int y, int flag, void *ustc);
+void exe_2() {
+	namedWindow("img");
+	setMouseCallback("img", on_mouce);
+	imshow("img", img);
+	while (true){
+		waitKey();
+	}
+}
+
+void on_mouce(int event, int x, int y, int flag, void *ustc) {
+	static Point pre_pt = Point(-1, -1);
+	char temp[16];
+	//String temp;
+	if (event == cv::EVENT_LBUTTONDOWN) {
+		pre_pt = Point(x, y);
+		stringstream ss_x;
+		stringstream ss_y;
+		ss_x << x;
+		ss_y << y;
+		//String temp = "(" + ss_x.str() + ", " + ss_y.str();
+		sprintf_s(temp, "x:%d,y:%d", x, y);
+		putText(img, temp, pre_pt, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1, 8);
+		imshow("img", img);
+	}
+}
+
+void on_mouce_rect(int event, int x, int y, int flag, void *ustc);
+void exe_3() {
+	namedWindow("img");
+	setMouseCallback("img", on_mouce_rect);
+	imshow("img", img);
+	while (true) {
+		waitKey();
+	}
+}
+
+void on_mouce_rect(int event, int x, int y, int flag, void *ustc) {
+	static Point pre_pt = Point(-1, -1);
+	static Point cur_pt = Point(-1, -1);
+	char temp[16];
+	//String temp;
+	if (event == cv::EVENT_LBUTTONDOWN) {
+		if (pre_pt.x == -1) {
+			pre_pt = Point(x, y);
+		}
+		
+		//stringstream ss_x;
+		//stringstream ss_y;
+		//ss_x << x;
+		//ss_y << y;
+		////String temp = "(" + ss_x.str() + ", " + ss_y.str();
+		//sprintf_s(temp, "x:%d,y:%d", x, y);
+		//putText(img, temp, pre_pt, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1, 8);
+		//imshow("img", img);
+	}
+	else if (event == cv::EVENT_MOUSEMOVE) {
+		if (pre_pt.x == -1) {
+			return;
+		}
+
+		cur_pt = Point(x, y);
+		Mat img_clone = img.clone();
+		rectangle(img_clone, Rect(pre_pt.x, pre_pt.y, abs(pre_pt.x - cur_pt.x), abs(pre_pt.y - cur_pt.y)), Scalar(0, 0, 255));
+
+		imshow("img", img_clone);
+	}
+	else if (event == cv::EVENT_LBUTTONUP ) {
+		pre_pt = Point(-1, -1);
+	}
+}
+
 void main(int argc, char** argv) {
-	exe_1();
+	exe_3();
 }
--- Chapter_8.h
+++ Chapter_8.h
@@ -1,2 +1,6 @@
 #pragma once
 void exe_1();
+void exe_2();
+void on_mouce();
+void exe_3();
+void on_mouce_rect();
